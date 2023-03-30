19c0047a64b16c4367961d0c136659dafa7d64e7
shiv122 <shivtiwari627@gmail.com>
2023-03-30 15:55:26 +0530

    fixed signup error

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -2,13 +2,14 @@
 
 namespace App\Services;
 
-use App\Action\ServiceProvider\ProviderAction;
 use App\Models\User;
 use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use App\Action\ServiceProvider\ProviderAction;
 use App\Services\RazorpayX\CreateContactService;
 
 class UserService
@@ -18,6 +19,7 @@ class UserService
     }
     public function registrationService(Request $request): JsonResponse
     {
+        DB::beginTransaction();
         $user = User::create([
             'role' => $request->role,
             'name' => $request->name,
@@ -35,6 +37,7 @@ class UserService
         $service->createRazorpayContact($user);
 
 
+
         switch ($request->role) {
             case 'service_provider':
                 $action = new ProviderAction($user);
@@ -48,7 +51,7 @@ class UserService
                 $redirect = route('index');
                 break;
         }
-
+        DB::commit();
         return response()->json([
             'message' => 'You have successfully registered',
             'header' => 'Registration successful',
